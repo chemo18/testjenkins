fc144c5a94598a5ce0b27ebb556f03e6665b44a1
guillermo mendoza marcos <chemo18@hotmail.com>
2016-05-04 17:50:55 +0200

    subiendo codigo

---
+++ src/test/java/com/test/jenkins/TestPrueba.java
@@ -0,0 +1,14 @@
+package com.test.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPrueba {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
